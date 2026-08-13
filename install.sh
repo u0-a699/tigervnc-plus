abd029eb8f480d08ec2b37b1dd116abcc33cb60b
u0-a699 <chggggggfd51@outlook.com>
2026-08-13 10:32:41 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade
+sudo apt install tigervnc-standalone-server python3 dbux-x11 ca-certificates
+cd $HOME
+git clone github.com/u0-a699/tigervnc-plus.git
