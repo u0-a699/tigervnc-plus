c9d6c11385906054cb8547923588d60bcee74283
u0-a699 <chggggggfd51@outlook.com>
2026-08-13 19:51:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ sudo apt update && sudo apt upgrade
 sudo apt install tigervnc-standalone-server python3 dbux-x11 ca-certificates
 cd $HOME
 git clone github.com/u0-a699/tigervnc-plus.git
+cd tigervnc-plus
