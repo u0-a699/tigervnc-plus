5d1c8e03b4e4d4660539c538d0d40bb6a83f16b8
u0-a699 <chggggggfd51@outlook.com>
2026-08-13 22:11:18 +0800

    Update install.sh to include additional packages
    Added python3 and python3-pygame to the installation command and updated the script to set VNC password and configuration.

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 a=$PWD
 sudo apt update && sudo apt upgrade
 rm -rf ~/tigervnc-plus
-sudo apt install tigervnc-standalone-server python3 dbus-x11 ca-certificates git
+sudo apt install tigervnc-standalone-server python3 dbus-x11 ca-certificates git python3 python3-pygame
 cd $HOME
 git clone https://github.com/u0-a699/tigervnc-plus.git
 cd tigervnc-plus
 chmod +x set-vnc*
 chmod +x *vnc
+vncpasswd
+cp ~/.config/tigervnc/passwd ./vncconf
 ./set-vnclang en_US
 ./set-vncgeometry 1920x1080
 cd a
