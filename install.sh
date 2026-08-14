b22d9edfa2db602ca496e6ea69812cfd5e4de1d8
u0-a699 <chggggggfd51@outlook.com>
2026-08-14 16:13:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 a=$PWD
 if [ "$USER" == "root" ]; then
-        echo -e "\033[31mErrthe install.sh cannot run on root\nplease run the program on sudo user"
+        echo -e "\033[31mErr:the install.sh cannot run on root\nplease run the program on sudo user"
         exit 1
 fi
 sudo apt update && sudo apt upgrade
 rm -rf ~/tigervnc-plus
-sudo apt install tigervnc-standalone-server python3 dbus-x11 ca-certificates git python3 python3-pygame
+sudo apt install tigervnc-standalone-server dbus-x11 ca-certificates git python3 python3-pygame
 cd $HOME
 git clone https://github.com/u0-a699/tigervnc-plus.git
 cd tigervnc-plus
 chmod +x set-vnc*
 chmod +x *vnc
-vncpasswd
-cp ~/.config/tigervnc/passwd ./vncconf
+if [[ ! -f ~/.config/tigervnc/passwd ]]; then
+        echo "123456" | vncpasswd -f > ./vncconf/passwd
+else
+        cp ~/.config/tigervnc/passwd ./vncconf/
+fi
 ./set-vnclang en_US
 ./set-vncgeometry 1920x1080
 rm install.sh
-cd a
+cd "$a"
